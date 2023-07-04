0f528f5090b950291a8f929cbf82751b8876c1c0
Vel MAr <velmar.annabell@gmail.com>
2023-07-04 21:13:03 +0300

    Recipe details, shopping list component

--- Section 3- Courese Project-The basics/the-basics-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Section 3- Courese Project-The basics/the-basics-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a test', 'https://hips.hearstapps.com/hmg-prod/images/stuffed-peppers-index-649c91e2e2259.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*'),
+    new Recipe('A test recipe', 'this is a test', 'https://hips.hearstapps.com/hmg-prod/images/stuffed-peppers-index-649c91e2e2259.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*'),
+      new Recipe('A test recipe', 'this is a test', 'https://hips.hearstapps.com/hmg-prod/images/stuffed-peppers-index-649c91e2e2259.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*'),
+]
   constructor() { }
 
   ngOnInit(): void {
